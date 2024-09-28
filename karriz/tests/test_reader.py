43e7ca8e4d84ec1c430568e1bd6dee4bdb92beae
karriz-dev <karriz@xymfor.us>
2024-09-28 17:10:56 +0900

    [test] 정상 경로에 있는 파일 로드 테스트 추가

--- karriz/tests/test_reader.py
+++ karriz/tests/test_reader.py
@@ -2,15 +2,21 @@ import unittest
 import mp3_file_reader
 
 class MP3FileReaderTests(unittest.TestCase):
-    # mp3 file이 존재 하지 않는 경로로 읽는다    
+    # [실패] mp3 file이 존재 하지 않는 경로로 읽는다    
     def test_new_mp3file_not_found(self):
         with self.assertRaises(FileNotFoundError):
-            self.mp3_file = mp3_file_reader.MP3FileReader("./test.mp3")
+            self.mp3_file_reader = mp3_file_reader.MP3FileReader("./test.mp3")
 
-    # file이 아닌 경로를 읽는다
+    # [실패] file이 아닌 경로를 읽는다
     def test_new_mp3file_read_directory(self):
         with self.assertRaises(IsADirectoryError):
-            self.mp3_file = mp3_file_reader.MP3FileReader("./assets")
+            self.mp3_file_reader = mp3_file_reader.MP3FileReader("./assets")
+
+    # [성공] 정상경로에 있는 파일을 읽는다
+    def test_new_mp3file_read(self):
+        self.mp3_file_reader = mp3_file_reader.MP3FileReader("./assets/input.mp3")
+
+        self.assertIsNotNone(self.mp3_file_reader.mp3_file)
 
     # 빈 MP3 파일을 읽는다
     def test_read_empty_file(self):
