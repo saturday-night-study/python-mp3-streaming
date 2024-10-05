9069dd81f9dab5673f02b902dc03bf29604c5fe9
gump <kineo2k@mrblue.com>
2024-10-05 18:47:35 +0900

    test: FileIO에서 파일 쓰기하는 실패 테스트 추가

--- gump/python_mp3_streaming/tests/test_file_io.py
+++ gump/python_mp3_streaming/tests/test_file_io.py
@@ -21,60 +21,60 @@ class TestFileIO(unittest.TestCase):
     # 메서드명 변경
     # 존재하는 파일이 입력된 경우 파일 객체를 반환하는지 확인
     def test_open_exists_file(self):
-        fio = FileIO(self.__exists_input_path)
+        fio = FileIO(self.__exists_input_path, "rb")
         self.assertFalse(fio.closed)
 
     # 존재하지 않는 파일이 입력된 경우 Error를 발생시키는지 확인
     def test_open_not_exists_file(self):
-        self.assertRaises(FileNotFoundError, FileIO, self.__not_exists_input_path)
+        self.assertRaises(FileNotFoundError, FileIO, self.__not_exists_input_path, "rb")
 
     # 파일 경로가 문자열이 아닌 타입으로 입력된 경우 Error를 발생시키는지 확인
     def test_open_invalid_parameter_type(self):
-        self.assertRaises(ValueError, FileIO, 123)
+        self.assertRaises(ValueError, FileIO, 123, "rb")
 
     # 파일 경로가 디렉터리일 경우 Error를 발생시키는지 확인
     def test_open_directory(self):
-        self.assertRaises(IsADirectoryError, FileIO, self.__directory_input_path)
+        self.assertRaises(IsADirectoryError, FileIO, self.__directory_input_path, "rb")
 
     # 파일 객체를 닫는지 확인
     def test_close(self):
-        fio = FileIO(self.__exists_input_path)
+        fio = FileIO(self.__exists_input_path, "rb")
         fio.close()
         self.assertTrue(fio.closed)
 
     def test_read(self):
-        fio = FileIO(self.__exists_input_path)
+        fio = FileIO(self.__exists_input_path, "rb")
 
         read_bytes = 4
         data: bytes = fio.read(read_bytes)
         self.assertEqual(len(data), read_bytes)
 
     def test_read_closed_file(self):
-        fio = FileIO(self.__exists_input_path)
+        fio = FileIO(self.__exists_input_path, "rb")
         fio.close()
 
         read_bytes = 1
         self.assertRaises(IOError, fio.read, read_bytes)
 
     def test_read_eof(self):
-        fio = FileIO(self.__empty_input_path)
+        fio = FileIO(self.__empty_input_path, "rb")
 
         read_bytes = 1
         self.assertRaises(EOFError, fio.read, read_bytes)
 
     def test_file_size(self):
-        fio = FileIO(self.__exists_input_path)
+        fio = FileIO(self.__exists_input_path, "rb")
         self.assertEqual(fio.file_size, self.__exists_input_file_size)
 
     def test_has_remain_bytes(self):
-        fio = FileIO(self.__exists_input_path)
+        fio = FileIO(self.__exists_input_path, "rb")
         self.assertTrue(fio.has_remain_bytes)
 
         fio.read(self.__exists_input_file_size)
         self.assertFalse(fio.has_remain_bytes)
 
     def test_current_position(self):
-        fio = FileIO(self.__exists_input_path)
+        fio = FileIO(self.__exists_input_path, "rb")
         self.assertEqual(fio.current_position, 0)
 
         read_bytes = 4
@@ -82,10 +82,14 @@ class TestFileIO(unittest.TestCase):
         self.assertEqual(fio.current_position, read_bytes)
 
     def test_skip(self):
-        fio = FileIO(self.__exists_input_path)
+        fio = FileIO(self.__exists_input_path, "rb")
         skip_bytes = 4
         fio.skip(skip_bytes)
         self.assertEqual(fio.current_position, skip_bytes)
+
+    def test_write(self):
+        fio = FileIO(self.__output_path, "wb")
+        fio.write(b"Hello, World!")
 
 # __main__ 변수는 모듈을 직접 실행하면 '__main__'이 되고, 임포트하면 모듈 이름이 됨
 if __name__ == '__main__':
