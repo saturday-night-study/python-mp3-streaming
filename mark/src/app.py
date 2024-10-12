34b1d72670e16148959d2ce4e68a7a0c89d3805f
MarkYoo23 <71996956+MarkYoo23@users.noreply.github.com>
2024-10-12 14:56:20 +0900

    feat : healthcheck 기능 추가 (green)

--- mark/src/app.py
+++ mark/src/app.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
