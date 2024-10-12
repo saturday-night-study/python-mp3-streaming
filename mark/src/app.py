98cd1b949d3a2b5fca2f0ed1750755a36728d416
MarkYoo23 <71996956+MarkYoo23@users.noreply.github.com>
2024-10-12 15:22:52 +0900

    feat : mp3 스트리밍 기능 추가 (green)

--- mark/src/app.py
+++ mark/src/app.py
@@ -1,7 +1,35 @@
+import io
+import os
+
 from fastapi import FastAPI
+from mp3 import MP3
+from mp3_file_io import MP3FileIo
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
+
+@app.get("/stream-mp3")
+async def stream_mp3():
+    path = "../resource/input.mp3"
+    mp3_io = MP3FileIo()
+    mp3_io.open(path)
+    mp3_io.read_all()
+    mp3_io.close()
+
+    mp3 = MP3(mp3_io)
+    mp3.set_header()
+    mp3.set_frame_size()
+    mp3.set_frame_count()
+    mp3.set_play_time()
+
+    headers = {
+        "Content-Disposition": f"inline; filename={os.path.basename(path)}",
+        "Content-Type": "audio/mpeg",
+        "Content-Length": str(mp3_io.get_size())
+    }
+
+    return StreamingResponse(io.BytesIO(mp3_io.get_all()), headers=headers)
